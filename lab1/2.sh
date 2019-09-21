83f5b08dcd855749170d69552cd0d9cc37506906
koalaa13 <nooshe981@gmail.com>
2019-09-21 14:26:07 +0300

    Fixed

--- lab1/2.sh
+++ lab1/2.sh
@@ -1,4 +1,31 @@
 #!/bin/bash
+reg='^[+-]?[0-9]+([.][0-9]+)?$'
+if [ $# -ne 3 ]
+then
+	echo "Wrong count of arguments"
+	exit 1
+fi
+
+if ! [[ $1 =~ $reg ]] 
+then
+	echo "Error: first argument is not a number"
+	exit 1
+fi
+
+
+if ! [[ $2 =~ $reg ]] 
+then
+	echo "Error: second argument is not a number"
+	exit 1
+fi
+
+
+if ! [[ $3 =~ $reg ]] 
+then
+	echo "Error: third argument is not a number"
+	exit 1
+fi
+
 res=$1
 if [ "$res" -lt "$2" ]
 then
